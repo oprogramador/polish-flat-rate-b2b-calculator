469cb9c401b983921722a56a7ffd2d8f02185ac9
oprogramador <uomodislesia@gmail.com>
2023-02-01 02:09:54 +0100

    2023 update

--- flatRate.js
+++ flatRate.js
@@ -7,16 +7,16 @@ const income = parseFloat(process.argv[2]);
 const rate = 0.12;
 
 /*
- * all the values below are only for 2022
+ * all the values below are only for 2023
  */
 
-const healthInsurance = 1007.81;
+const healthInsurance = 1128.48;
 const healthInsuranceToReduce = 0;
 
-const retirementInsurance = 693.58;
-const disabilityInsurance = 284.26;
-const accidentInsurance = 59.34;
-const workFund = 87.05;
+const retirementInsurance = 812.23;
+const disabilityInsurance = 332.88;
+const accidentInsurance = 69.49;
+const workFund = 101.94;
 
 const totalSocialInsurance = retirementInsurance + disabilityInsurance + accidentInsurance;
 const taxableIncome = Math.round(income - totalSocialInsurance);
