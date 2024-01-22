eea8fa0b9de11977f9c5ced6301791a8a8883381
oprogramador <uomodislesia@gmail.com>
2024-01-22 20:00:32 +0100

    2024 update

--- flatRate.js
+++ flatRate.js
@@ -10,13 +10,13 @@ const rate = 0.12;
  * all the values below are only for 2023
  */
 
-const healthInsurance = 1128.48;
+const healthInsurance = 1258.39;
 const healthInsuranceToReduce = 0;
 
-const retirementInsurance = 812.23;
-const disabilityInsurance = 332.88;
-const accidentInsurance = 69.49;
-const workFund = 101.94;
+const retirementInsurance = 916.35;
+const disabilityInsurance = 375.55;
+const accidentInsurance = 78.40;
+const workFund = 115.01;
 
 const totalSocialInsurance = retirementInsurance + disabilityInsurance + accidentInsurance;
 const taxableIncome = Math.round(income - totalSocialInsurance);
