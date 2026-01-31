0f9a60024a1e3e80dfd3f0786cd2b3b127f6a11b
Piotr <uomodislesia@gmail.com>
2026-01-31 11:48:35 +0100

    2026

--- flatRate.js
+++ flatRate.js
@@ -7,16 +7,18 @@ const income = parseFloat(process.argv[2]);
 const rate = 0.12;
 
 /*
- * all the values below are only for 2025
+ * There is no guarentee of any correctness of this file (or this entire repo)
+ * including numbers and business logic.
+ * All the values below are only for the year 2026.
  */
 
-const healthInsurance = 1384.97;
+const healthInsurance = 1495.04;
 const healthInsuranceToReduce = 0;
 
-const retirementInsurance = 1015.78;
-const disabilityInsurance = 416.30;
-const accidentInsurance = 86.90;
-const workFund = 127.49;
+const retirementInsurance = 1103.27;
+const disabilityInsurance = 452.16;
+const accidentInsurance =  94.39;
+const workFund = 138,47;
 
 const totalSocialInsurance = retirementInsurance + disabilityInsurance + accidentInsurance;
 const taxableIncome = Math.round(income - totalSocialInsurance);
